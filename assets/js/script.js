3a5a6f2fdfdd0b59a24ab08fcd4bb5df8f9482ae
Daniel Denton <danielryandenton@gmail.com>
2021-10-14 13:24:08 -0500

    implement textarea on click, replace with p on blur retaining value input

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,6 +43,33 @@ var currentHour = function () {
 };
 var hour = currentHour();
 
+// render input on click
+$(".container").on("click", ".text-container", function () {
+  // if no children elements
+  if ($(this).children().length === 0) {
+    $(this).append("<textarea>");
+    $(this).children("textarea").trigger("focus");
+  }
+  // if child element is present
+  if ($(this).children(".description").length === 1) {
+    var value = $(this).children(".description").html();
+
+    $(this)
+      .children(".description")
+      .replaceWith("<textarea>" + value + "</textarea>");
+
+    $(this).children("textarea").trigger("focus");
+  }
+
+  // when user clicks off textarea
+  $(this)
+    .children("textarea")
+    .on("blur", function () {
+      var value = $(this).val();
+      $(this).replaceWith("<p class='description'>" + value + "</p>");
+    });
+});
+
 // render colors on load
 setBackgroundColor(hour);
 
